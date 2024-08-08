156b0a2a19f9f669d13c46e46461e153edda293d
Aymen Maachi <kraymis.maachi@gmail.com>
2024-08-08 03:06:20 +0100

    another random commit hhh

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,19 +18,19 @@ const NavBar = ({ scrollToSection, homeRef, projectsRef, servicesRef, whymeRef, 
             </div>
             <ul className={`flex flex-col md:flex-row md:space-x-8 items-center md:justify-end space-y-4 md:space-y-0 md:gap-8 mt-4 md:mt-0 ${isOpen ? 'block' : 'hidden'} md:flex w-full`}>
                 <li className="w-full md:w-auto">
-                    <button onClick={() => scrollToSection(homeRef)} className="hover:text-[#7456FF] text-xl md:text-xl md:font-semibold w-full text-center">Home</button>
+                    <button onClick={() => scrollToSection(homeRef)} className="hover:text-[#7456FF] text-xl md:text-lg md:font-semibold w-full text-center">Home</button>
                 </li>
                 <li className="w-full md:w-auto">
-                    <button onClick={() => scrollToSection(projectsRef)} className="hover:text-[#7456FF] text-xl md:text-xl md:font-semibold w-full text-center">Projects</button>
+                    <button onClick={() => scrollToSection(projectsRef)} className="hover:text-[#7456FF] text-xl md:text-lg md:font-semibold w-full text-center">Projects</button>
                 </li>
                 <li className="w-full md:w-auto">
-                    <button onClick={() => scrollToSection(servicesRef)} className="hover:text-[#7456FF] text-xl md:text-xl md:font-semibold w-full text-center">Services</button>
+                    <button onClick={() => scrollToSection(servicesRef)} className="hover:text-[#7456FF] text-xl md:text-lg md:font-semibold w-full text-center">Services</button>
                 </li>
                 <li className="w-full md:w-auto">
-                    <button onClick={() => scrollToSection(whymeRef)} className="hover:text-[#7456FF] text-xl md:text-xl md:font-semibold w-full text-center">Why Me</button>
+                    <button onClick={() => scrollToSection(whymeRef)} className="hover:text-[#7456FF] text-xl md:text-lg md:font-semibold w-full text-center">Why Me</button>
                 </li>
                 <li className="w-full md:w-auto">
-                    <button onClick={() => scrollToSection(contactRef)} className="border border-[#7456FF] py-2 px-4 rounded text-[#7456FF] hover:bg-[#7456FF] hover:text-white text-xl md:text-xl md:font-semibold w-full text-center">Contact Me</button>
+                    <button onClick={() => scrollToSection(contactRef)} className="border border-[#7456FF] py-2 px-4 rounded text-[#7456FF] hover:bg-[#7456FF] hover:text-white text-xl md:text-lg md:font-semibold w-full text-center">Contact Me</button>
                 </li>
             </ul>
         </nav>
